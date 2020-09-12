7001a1e9bfe261498f68f0610200a3b43e395cc5
Kilian Agboton <kilian.agboton@gmail.com>
2020-09-12 04:30:33 -0400

    first commit :: add Header and StartGameScreen components

---
+++ components/StartGameScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+
+const StartGameScreen = props => {
+
+    return(
+
+        <View style={styles.screen}>
+            <Text>Game starting !</Text>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    screen : {
+        flex : 1,
+        padding : 10,
+        alignItems: 'center',
+
+    }
+
+})
+
+export default StartGameScreen
