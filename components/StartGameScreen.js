4e344cc40f14acfb9212bbc69066f8c66e8ecf60
Kilian Agboton <kilian.agboton@gmail.com>
2020-09-12 04:52:45 -0400

    styling a view as Card container using drop shadows and rounder corners

--- components/StartGameScreen.js
+++ components/StartGameScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native'
 
 const StartGameScreen = props => {
 
     return(
 
         <View style={styles.screen}>
-            <Text>Game starting !</Text>
+            <Text style = {styles.title}>Game starting !</Text>
+            <View style={styles.inputContainer}>
+                <Text >Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset"/>
+                    <Button title="Confirm"/>   
+                </View>     
+            </View>
         </View>
 
     );
@@ -17,6 +25,31 @@ const styles = StyleSheet.create({
         flex : 1,
         padding : 10,
         alignItems: 'center',
+
+    },
+    title : {
+        fontSize : 20,
+        marginVertical : 10,
+         
+    },
+    buttonContainer : {
+        flexDirection : 'row',
+        width : '100%',
+        justifyContent : 'space-between',
+        paddingHorizontal : 15
+        
+    },
+    inputContainer : {
+        width : 300,
+        maxWidth: '80%',
+        alignItems: "center",
+        shadowColor : 'black',
+        shadowOffset : {width : 0, height : 2},
+        shadowRadius : 6,
+        shadowOpacity : 0.26,
+        elevation : 5,
+        backgroundColor : 'white', 
+        padding : 20
 
     }
 
